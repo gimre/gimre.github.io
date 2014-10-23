ad15ccb17c90e9995e7a07525bd652a0e73e0820
gimre <imregabriel@gmail.com>
2014-10-24 00:16:27 +0300

    added navbar and mock data

--- js/app.js
+++ js/app.js
@@ -3,4 +3,30 @@
 
 	var app = angular.module( 'blog', [ ] );
 
+	app.run( [
+		'$rootScope',
+		function ( $rootScope ) {
+			$rootScope.prettyDate = function ( date ){
+				var	diff = (((new Date()).getTime() - date.getTime()) / 1000),
+					day_diff = Math.floor(diff / 86400);
+						
+				if ( isNaN(day_diff) || day_diff < 0 )
+					return;
+
+				if ( day_diff >= 31 )
+					return 'a long time ago';
+						
+				return day_diff == 0 && (
+						diff < 60 && "just now" ||
+						diff < 120 && "1 minute ago" ||
+						diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+						diff < 7200 && "1 hour ago" ||
+						diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+					day_diff == 1 && "Yesterday" ||
+					day_diff < 7 && day_diff + " days ago" ||
+					day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+			};
+		}
+	] );
+
 } )( );
--- js/controllers/post-list.js
+++ js/controllers/post-list.js
@@ -8,10 +8,10 @@
 
 			$scope.model = {
 				posts: [
-					{ title: 'My first post' },
-					{ title: 'My second post' },
-					{ title: 'My third post' },
-					{ title: 'My fourth post' }
+					{ title: 'My first post', date: new Date( ), body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste id vel sequi earum numquam sapiente. Est velit accusantium nostrum aspernatur error ea ut, perferendis temporibus, ipsa quae, inventore repellat mollitia.' },
+					{ title: 'My second post', date: new Date( Date.now( ) - 6e4 ), body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum perspiciatis quis, incidunt unde atque accusamus. Quas porro, eum reprehenderit! Vitae ullam nobis harum sapiente voluptatum cupiditate, eos delectus, quidem quas!' },
+					{ title: 'My third post', date: new Date( Date.now( ) - 6e5 ), body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero accusantium iure consectetur minima error vitae eligendi iusto maiores, repudiandae fuga doloremque, beatae odio nisi consequuntur, sit et pariatur ullam aspernatur!' },
+					{ title: 'My fourth post', date: new Date( Date.now( ) - 6e6 ), body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nobis illo quibusdam ducimus voluptates nesciunt, repudiandae similique cum eveniet doloribus iste vitae, corporis porro fugiat aut, mollitia vero beatae incidunt.' }
 				]
 			};
 
