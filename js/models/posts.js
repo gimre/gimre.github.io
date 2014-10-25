f83341a2884bf336022de94682d43272aea5643e
gimre <imregabriel@gmail.com>
2014-10-25 23:30:58 +0300

    model fix

--- js/models/posts.js
+++ js/models/posts.js
@@ -4,8 +4,8 @@
 	angular.module( 'blog' ).service( 'postsModel', [ '$http', function ( $http ) {
 		return {
 			getAll: function ( ) {
-				return $http.get( '/api/posts.json' ).then( function ( data ) { 
-					data.forEach( function ( post ) {
+				return $http.get( '/api/posts.json' ).then( function ( response ) { 
+					response.data.forEach( function ( post ) {
 						var parts = post.date.split(' '),
 							date  = parts.shift( ),
 							time  = parts.shift( ),
@@ -14,7 +14,7 @@
 						post.date = Date.parse( date + 'T' + time + zone.slice( 0, zone.length - 2 ) + ':' + zone.slice( -2 ) );
 					} );
 
-					return data;
+					return response.data;
 				} );
 			}
 		}
