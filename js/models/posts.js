415746075c42de9e603157d89593ed2563b1062e
gimre <imregabriel@gmail.com>
2014-10-25 23:05:03 +0300

    linked posts model to main view controller

--- js/models/posts.js
+++ js/models/posts.js
@@ -5,9 +5,16 @@
 		return {
 			getAll: function ( ) {
 				return $http.get( '/api/posts.json' ).then( function ( data ) { 
-					return data.map( function ( post ) {
-						// convert date to JS date
+					data.forEach( function ( post ) {
+						var parts = post.date.split(' '),
+							date  = parts.shift( ),
+							time  = parts.shift( ),
+							zone  = parts.shift( );
+						
+						post.date = Date.parse( date + 'T' + time + zone.slice( 0, zone.length - 2 ) + ':' + zone.slice( -2 ) );
 					} );
+
+					return data;
 				} );
 			}
 		}
